0e0413ad4b671582571883a6e8e897df4da2f868
hccazado <heitorcazado@gmail.com>
2021-11-17 23:40:43 -0300

    trabalhando post cadastrar

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,4 +1,5 @@
-const { validationResult } = require("express-validator");
+const { validationResult, body } = require("express-validator");
+const {Sequelize, Usuario} = require("../models");
 
 const usuarioController = {
 	getUsuarioIndex: (req, res) => {
@@ -7,9 +8,17 @@ const usuarioController = {
 	getCadastrar: (req, res, next) => {
 		res.render("cadastrar");
 	},
-	postCadastrar: (req, res, next) => {
+	postCadastrar: async (req, res, next) => {
 		const errors = validationResult(req);
-		console.log(req.body);
+		var dados = req.body
+		if(errors.isEmpty()){
+			if(dados.senha === dados.confirmarSenha){
+				console.log("entrou sem errors");
+				let usuarioCadastrado = await Usuario.create(dados);
+				return res.send(usuarioCadastrado);
+			}
+			
+		}
 		console.log(errors);
 	},
 };
--- middlewares/validator.js
+++ middlewares/validator.js
@@ -4,8 +4,7 @@ const validaCamposCadastoUsuario = [
     check('nome').trim().notEmpty().withMessage("Não há nome").bail().isLength({min:3}).withMessage("Nome muito curto"),
     check('sobrenome').trim().notEmpty().withMessage("Não há sobrenome").bail().isLength({min:3}).withMessage("sobrenome muito curto"),
     check("email").trim().notEmpty().withMessage("Informar email").bail().isEmail().withMessage("Informar Email valido"),
-    check('senha').trim().notEmpty().withMessage("Informar senha de acesso").bail().isLength({min:6}).withMessage("pelo menos 6 caracteres"),
-    check("confirmarSenha").trim().notEmpty().withMessage("Informar senha de acesso").bail().isLength({min:6}).withMessage("pelo menos 6 caracteres").equals('senha').withMessage("As senhas não coincidem"),
+    check('senha').trim().notEmpty().withMessage("Informar senha de acesso").bail().isLength({min:6}).withMessage("pelo menos 6 caracteres")
 ];
 
 module.exports = {

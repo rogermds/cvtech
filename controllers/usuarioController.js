d934ff15f19a6e10ebf7a9eb99771e7b40be217c
hccazado <heitorcazado@gmail.com>
2021-11-19 17:52:47 -0300

    criando metodo post para editar usuario

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,4 +1,5 @@
 const {Usuario, Curriculo, sequelize} = require("../models/");
+const fs = require('fs');
 
 const usuarioController = {
 	getUsuarioIndex: async (req, res) => {
@@ -19,6 +20,25 @@ const usuarioController = {
 		// res.send(editarUsuario);
 	},
 	postEditarUsuario: (req, res) => {
+		let { id } = req.session.user;
+		let novosDados = req.body;
+		let editarUsuario = await Usuario.findByPk(id)
+		let oldAvatar = editarUsuario.dataValues.avatar;
+		if(!req.file){
+			editarUsuario.update(novosDados);
+		}
+		else{
+			if(oldAvatar.length > 0){
+				fs.unlink('../public/images/avatar'+oldAvatar,function(err){
+					if(err) return console.log(err);
+					console.log('Avatar antigo removido');
+			   }); 
+			}
+			novosDados.avatar = req.file.filename;
+			editarUsuario.update(novosDados);
+		}
+		
+		
 		res.render("editar-usuario");
 	},
 };

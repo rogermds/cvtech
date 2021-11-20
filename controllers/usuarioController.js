4d1af389687bcb64df7a9a6a87548dc11d409908
hccazado <heitorcazado@gmail.com>
2021-11-19 21:51:56 -0300

    finalizado editar metodo editarusuario, corrigido visualizarCV

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,5 +1,7 @@
 const {Usuario, Curriculo, sequelize} = require("../models/");
 const fs = require('fs');
+const bcrypt = require("bcryptjs");
+const path = require("path");
 
 const usuarioController = {
 	getUsuarioIndex: async (req, res) => {
@@ -21,24 +23,53 @@ const usuarioController = {
 	postEditarUsuario: async (req, res) => {
 		let { id } = req.session.user;
 		let novosDados = req.body;
-		let editarUsuario = await Usuario.findByPk(id)
+		novosDados.senha = bcrypt.hashSync(novosDados.senha, 10);
+		let arquivo = req.file;
+		let editarUsuario = await Usuario.findByPk(id);
 		let oldAvatar = editarUsuario.dataValues.avatar;
 		if(!req.file){
+			novosDados.avatar = oldAvatar;
 			editarUsuario.update(novosDados);
-		}
-		else{
-			if(oldAvatar.length > 0 && req.file){
-				fs.unlink('../public/images/avatar'+oldAvatar,function(err){
-					if(err) return console.log(err);
-					console.log('Avatar antigo removido');
-			   }).then(_=>{
-				novosDados.avatar = req.file.filename;
-				editarUsuario.update(novosDados);
-			   }) 
+			let novaSession = {
+				id: id,
+				...novosDados
 			}
+			delete novaSession.senha;
+			delete novaSession.confirmarSenha;
+			req.session.user = novaSession;
+			return res.redirect("/usuario");
+		}
+		if(oldAvatar && req.file){
+			fs.unlink(path.resolve('public/images/avatar',oldAvatar), (err)=>{
+				if(err) {
+					return console.log(err);
+				}
+				console.log('Avatar antigo removido');
+			})
+			novosDados.avatar = arquivo.filename;
+			editarUsuario.update(novosDados);
 			
-		}		
-		res.render("editar-usuario");
+			let novaSession = {
+				id: id,
+				...novosDados
+			}
+			delete novaSession.senha;
+			delete novaSession.confirmarSenha;
+			req.session.user = novaSession;
+
+			return res.redirect("/usuario");
+		}
+		novosDados.avatar = arquivo.filename;
+		editarUsuario.update(novosDados);
+		let novaSession = {
+			id: id,
+			...novosDados
+		}
+		delete novaSession.senha;
+		delete novaSession.confirmarSenha;
+		req.session.user = novaSession;
+		
+		return res.redirect("/usuario");	
 	},
 };
 
